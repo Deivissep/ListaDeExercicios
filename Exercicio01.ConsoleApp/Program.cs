a55320dbe50bb8e256cf2ee9b3bb219d76e3a922
Davi Gonçalves <daavi2223@gmail.com>
2026-04-02 19:54:01 -0300

    Primeiro Exercicio de Calcula de Caixa Retangular

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crie um programa para calcular as dimensões de uma caixa retangular//
+// Comprimento x Largura x Altura//
+// (Formula V = C . L . A) 50cm (Comprimento) X 30cm (Largura) x 20cm (Altura)
+
+Console.WriteLine("Digite as dimensões da caixa retangular: ");
+
+double comprimento = Convert.ToDouble(Console.ReadLine());
+double largura = Convert.ToDouble(Console.ReadLine());
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double volume = comprimento * largura * altura;
+
+
+Console.WriteLine($"O Volume da caixa é: {volume:F2} cm3");
+
+Console.ReadLine();
---
+++ Exercicio02.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+2. Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma
+viagem.
+O programa deve solicitar ao usuário:
+a. A quilometragem inicial do veículo no início da viagem.
+b. A quilometragem final ao término da viagem.
+c. A quantidade de combustível consumida durante a viagem (em litros).
+*/
+
+Console.WriteLine("Digite a Distância da Viagem e a quantidade de Combustivel: ");
+
+double quilometragemInicial = Convert.ToDouble(Console.ReadLine());
+double quilometragemFinal = Convert.ToDouble(Console.ReadLine());
+double quantidadeCombustivel = Convert.ToDouble(Console.ReadLine());
+
+double combustivelGasto = quilometragemInicial + quilometragemFinal / quantidadeCombustivel;
+
+Console.WriteLine($"A quantidade de combustivel consumida é: {combustivelGasto:F2} ");
+
+Console.ReadLine();
